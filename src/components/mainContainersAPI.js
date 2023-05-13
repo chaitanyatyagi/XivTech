40e79f1559ede288a99e40ff338abadd01ca44e2
Chaitanya Tyagi <chaitanyatyagi1540@gmail.com>
2023-05-13 20:41:43 +0530

    last animation left

--- src/components/mainContainersAPI.js
+++ src/components/mainContainersAPI.js
@@ -3,25 +3,29 @@ const data = [
         head: "AI + RPA is what we do",
         button: "AI + RPA Automation",
         body: "Future-Proof your business. Drive efficiency, profitability and deliver on customer experience",
-        image: "https://www.xivtech.io./rp2.jpg"
+        image: "https://www.xivtech.io./rp2.jpg",
+        animation: "slide-up"
     },
     {
         head: "Make Bolder Choices",
         button: "Build Better Apps",
         body: "Digital focused strategies to realize market-changing ideas",
-        image: "https://www.xivtech.io./p1.png"
+        image: "https://www.xivtech.io./p1.png",
+        animation: "slide-up"
     },
     {
         head: "Innovate with Speed",
         button: "DevOps",
         body: "Create higher quality software, deliver on customer expectations and business goals",
-        image: "https://www.xivtech.io./p2.jpg"
+        image: "https://www.xivtech.io./p2.jpg",
+        animation: "slide-up"
     },
     {
         head: "Embrace Cloud",
         button: "Cloud Services",
         body: "With Cloud-First accelerate innovation and optimize performance",
-        image: "https://www.xivtech.io./p3.jpg"
+        image: "https://www.xivtech.io./p3.jpg",
+        animation: "slide-up"
     }
 ]
 
